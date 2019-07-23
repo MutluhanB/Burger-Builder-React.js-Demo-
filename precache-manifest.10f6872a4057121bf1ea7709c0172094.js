da233f00542433b4c927ab644e1dde05e5084678
mutluhan <mutluhan43@gmail.com>
2019-07-23 17:35:44 +0300

    Updates

---
+++ precache-manifest.10f6872a4057121bf1ea7709c0172094.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f42d7c5645e40d3bd3734c00952c3553",
+    "url": "/Burger-Builder-React.js-Demo-/index.html"
+  },
+  {
+    "revision": "ae0af2a850e78b79c166",
+    "url": "/Burger-Builder-React.js-Demo-/static/css/main.34406b3a.chunk.css"
+  },
+  {
+    "revision": "d1763b2cbbeb04657623",
+    "url": "/Burger-Builder-React.js-Demo-/static/js/2.4c530373.chunk.js"
+  },
+  {
+    "revision": "ae0af2a850e78b79c166",
+    "url": "/Burger-Builder-React.js-Demo-/static/js/main.4ecaa93f.chunk.js"
+  },
+  {
+    "revision": "1256aabbce0c67d70af8",
+    "url": "/Burger-Builder-React.js-Demo-/static/js/runtime~main.3c1f74aa.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/Burger-Builder-React.js-Demo-/static/media/burger-logo.b8503d26.png"
+  }
+]);
